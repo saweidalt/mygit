0cb271a6fda11304995c9d6648b0451571eb9533
inc <saweidalt@126.com>
2016-09-21 22:35:38 -0700

    update handlers

---
+++ myweb/www/config_default.py
@@ -0,0 +1,16 @@
+# config_default.py
+
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'www',
+        'password': 'www',
+        'db': 'awesome'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
